e994d6560dd598d2811714a5b501bbe74bc40598
XicoCarCol <alex_r_9498@hotmail.com>
2021-06-06 18:02:58 -0500

    Se actualiza archivo para db

--- SCRIPTDB.sql
+++ SCRIPTDB.sql
@@ -1,18 +1,22 @@
 /*SCRIPT PARA LA BASE DE DATOS*/
 
-create database pruebaBwl; /*Crea base de datos*/
-use pruebaBwl; /*se ocupa la base dedatos*/
+create
+database pruebaBwl; /*Crea base de datos*/
+use
+pruebaBwl; /*se ocupa la base dedatos*/
 
 
-create table Usuarios(
-id_usuario int NOT NULL AUTO_INCREMENT,
-correo varchar(100) NOT NULL,
-passUno varchar(20) NOT NULL,
-passDos varchar(20) NOT NULL,
-nombre varchar(100) NOT NULL,
-ultimoLogin datetime NOT NULL,
-fechaReg datetime NOT NULL,
-PRIMARY KEY ( id_usuario ));
+create table usuarios
+(
+    id_usuario   int          NOT NULL AUTO_INCREMENT,
+    correo       varchar(100) NOT NULL,
+    pass_uno     varchar(20)  NOT NULL,
+    pass_dos     varchar(20)  NOT NULL,
+    nombre       varchar(100) NOT NULL,
+    ultimo_login datetime     NOT NULL,
+    fecha_reg    datetime     NOT NULL,
+    PRIMARY KEY (id_usuario)
+);
 
 
 
